d73fd947d6ae275efb39f077ba69b964cf81f9ea
Thompson1608 <nguoiay1608@gmail.com>
2023-02-14 23:30:35 +0700

    update function bnt_update

--- demo.js
+++ demo.js
@@ -7,12 +7,9 @@
 // //     text += "<li>" + fruits[i] + "</li>";
 // }
 
-
-
-var a = document.getElementById('a')
-var b = document.getElementById('b')
-var c = document.getElementById('result')
-
+var a = document.getElementById("a");
+var b = document.getElementById("b");
+var c = document.getElementById("result");
 
 // function getlog(){
 // //alert(Number(a.value) + Number( b.value))
@@ -26,7 +23,7 @@ var c = document.getElementById('result')
 
 // for (let i = 0; i < alen; i++) {
 //    text += '<li>' + arr[i] + '</li>';
-    
+
 // }
 // text +='</ul>';
 
@@ -34,15 +31,13 @@ var c = document.getElementById('result')
 
 // function myFunction(value) {
 //     text += "<li>" + value + "</li>";
-//   } 
-
-
+//   }
 
 var arr = [
-    {id:1,name:'test1',classItem:'a1'},
-    {id:2,name:'test2',classItem:'a1'},
-    {id:3,name:'test3',classItem:'a1'},
-    {id:4,name:'test4',classItem:'a1'},
+  { id: 1, name: "test1", classItem: "a1" },
+  { id: 2, name: "test2", classItem: "a1" },
+  { id: 3, name: "test3", classItem: "a1" },
+  { id: 4, name: "test4", classItem: "a1" },
 ];
 
 // var html = '';
@@ -53,16 +48,15 @@ var arr = [
 //      '<td>' + arr[i].name + '</td>';
 //      '<td>' + arr[i].class + '</td>';
 //     '</tr>'
-    
+
 // }
 
 // document.getElementById('data').innerHTML = html
 
-
-function renderTable(){
-    var html = '';
-for (const item of arr) {
-    html+= `<tr>
+function renderTable() {
+  var html = "";
+  for (const item of arr) {
+    html += `<tr>
     <td>${item.id}</td>
     <td>${item.name}</td>
     <td>${item.classItem}</td>
@@ -70,75 +64,76 @@ for (const item of arr) {
     <button onclick='deleteItem(${item.id})'>delete</button>
     <button onclick='Edit(${item.id})'>Edit</button>
     </td>
-    </tr>`
-}
-var tbody = document.querySelector('table tbody');
+    </tr>`;
+  }
+  var tbody = document.querySelector("table tbody");
 
-tbody.innerHTML = html;
-
+  tbody.innerHTML = html;
 }
 
 renderTable();
 
-function deleteItem(id){
-    // var index = arr.findIndex ((item)=>{
-    //     return item.id === id
-    // })
-    arr.splice(id-1,1);
-    renderTable();
+function deleteItem(id) {
+  // var index = arr.findIndex ((item)=>{
+  //     return item.id === id
+  // })
+  arr.splice(id - 1, 1);
+  renderTable();
 }
-
 
 function addnew() {
-    var ip = document.getElementsByTagName('input');
-    var newObj = {
-        id:arr.length + 1,
-        name: ip[0].value,
-        classItem: ip[1].value,
+  var ip = document.getElementsByTagName("input");
+  var newObj = {
+    id: arr.length + 1,
+    name: ip[0].value,
+    classItem: ip[1].value,
+  };
+  arr.push(newObj);
+  ip[0].value = "";
+  ip[1].value = "";
+  renderTable();
+}
+/**
+ * Edit value of
+ * @param {*} id
+ */
+function Edit(id) {
+  var ip = document.getElementsByTagName("input");
+  ip[2].setAttribute("value", id);
+  var itemEdit = null;
+  for (const item of arr) {
+    if (item.id === id) {
+      itemEdit = item;
+      break;
     }
-    arr.push(newObj);
-    ip[0].value = '';
-    ip[1].value = '';
-    renderTable();
+  }
+  var { name, classItem } = itemEdit;
+  ip[0].value = name;
+  ip[1].value = classItem;
 }
 
+var btn_update = document.getElementById("btn-update");
+/**
+ * handle function of button update when click
+ */
+btn_update.onclick = function () {
+  var allInput = document.getElementsByTagName("input");
+  var indexID = allInput[2].value;
 
+  var index = arr.findIndex((item) => {
+    return item.id == indexID;
+  });
 
-function Edit(id){
-    var ip = document.getElementsByTagName('input');
-   ip[2].setAttribute('value', id);
-   var itemEdit = null;
-   for (const item of arr) {
-        if (item.id=== id) {
-            itemEdit = item
-            break;
-        }
-    
-   }
-   var {name, classItem} = itemEdit;
-   ip[0].value = name;
-   ip[1].value = classItem;
-}
+  //   arr[index].id = ip;
+  arr[index].name = allInput[0].value;
+  arr[index].classItem = allInput[1].value;
+  localStorage.setItem("data", JSON.stringify(arr));
+  renderTable(arr);
 
-var btn_update = document.getElementById('btn-update');
-
-btn_update.onclick = function(){
-    var ip = document.getElementsByTagName('input');
-    var ip = ip[2].value
-
-    var index = arr.findIndex((item)=>{
-        return item.id ==id
-    })
-    
-    arr[index].id = id;
-    arr[index].name = ip[1].value;
-    localStorage.setItem('data',JSON.stringify(arr))
-    renderTable(arr);
-
-    ip[0].value = '';
-    ip[1].value = '';
-    console.log(this.id)
-}
+  allInput[0].value = "";
+  allInput[1].value = "";
+  console.log(this.id);
+};
 
 // function upd(id){
 //     var z = id;
@@ -147,7 +142,7 @@ btn_update.onclick = function(){
 //             ip[0].value = namees ;
 //             ip[1].value = classItem;
 //         }
-        
+
 //     }
 //     console.log(z)
 // }
@@ -156,5 +151,4 @@ btn_update.onclick = function(){
 //     ip[z-1].value = namees ;
 //     ip[z-1].value = classItem;
 
-    
 // }
